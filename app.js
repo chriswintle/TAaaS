0ea11f8bc7bb321ca764113e783509722ec4cf28
Chris Wintle <cwintle@andigital.com>
2016-04-13 12:13:19 +0100

    put bootstrapping back in

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ var service = new AdviceService();
 
 var app = express();
 
-
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -28,7 +30,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
 
+function bootstrapData(){
+  service.getAllData().then(function(result){
+    if(!result || result.length < 1){
+      console.log("BOOSTRAPPING DATA")
+      var obj = JSON.parse(fs.readFileSync('./data/advice.json', 'utf8')).advice;
 
+      for (var i = obj.length - 1; i >= 0; i--) {
+        var result = obj[i];
+        console.log("ADDING ITEM");
+        service.addAdvice(result.tip);
+      }
+    }
+  })
+}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -41,7 +56,7 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-
+if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
@@ -49,17 +64,17 @@ app.use(function(req, res, next) {
       error: err
     });
   });
+}
 
-
-// // production error handler
-// // no stacktraces leaked to user
-// app.use(function(err, req, res, next) {
-//   res.status(err.status || 500);
-//   res.render('error', {
-//     message: err.message,
-//     error: {}
-//   });
-// });
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 
 app.listen(8080, function () {
   console.log('App Listening on port 8080!');
