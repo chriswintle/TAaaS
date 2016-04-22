97e2f2e6803243b6c1856f1ef6998cfa7f53177a
Chris Wintle <cwintle@andigital.com>
2016-04-22 09:17:43 +0100

    add logging in to see wagawan with dokku

--- services/schema/advice.js
+++ services/schema/advice.js
@@ -1,6 +1,7 @@
 var mongoose = require("mongoose");
 var config = require("../../config/config");
 if(config){
+	console.log("ATTEMPTING TO CONNECT TO MONGO ON: "+"mongodb://"+config.mongo_url+'/advice')
 	mongoose.connect("mongodb://"+config.mongo_url+'/advice');
 
 }
